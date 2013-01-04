7279c57ea0bc0dc6c026eaf86372525add795d57
James Scott <james.scott@mccombs.utexas.edu>
2013-01-04 12:33:41 -0600

    Minor changes.

--- Chapter01/examples/ozone.R
+++ Chapter01/examples/ozone.R
@@ -1,4 +1,5 @@
 # Load the library
+# you might have to install this the first time
 library(mlbench)
 
 # Load the data
@@ -12,8 +13,22 @@ ozone = data(Ozone, package='mlbench')
 ozone = na.omit(Ozone)[,4:13]
 
 y = ozone[,1]
-y = y - mean(y)
 x = as.matrix(ozone[,2:10])
+
+# add an intercept
+x = cbind(1,x)
 
 # compute the estimator
 betahat = solve(t(x) %*% x) %*% t(x) %*% y
+
+# Fill in the blank
+# betacov = ?
+
+# Now compare to lm
+# the 'minus 1' notation says not to fit an intercept (we've already hard-coded it as an extra column)
+lm1 = lm(y~x-1)
+
+summary(lm1)
+betacovlm = vcov(lm1)
+sqrt(diag(betacovlm))
+
